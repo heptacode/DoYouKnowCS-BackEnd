7b432d9b3eb3aa35edc17e8994b846d4f564cf71
heptacode <admin@hyunwoo.kim>
2019-11-27 16:39:10 +0900

    Finish getAllMeal

--- src/lib/getMeal.ts
+++ src/lib/getMeal.ts
@@ -1,91 +1,31 @@
-/*
-{
-    "next_token": 20,
-    "articles": [
-      {
-        "id": 104470953,
-        "organization_id": 17195,
-        "organization_name": "선린인터넷고등학교",
-        "organization_logo": "https://iam-organization-r.akamaized.net/logo-image/17195/qa8AluKbzFCNKhOV.png",
-        "group_id": 2071367,
-        "group_name": "오늘의 급식",
-        "title": "11월 29일 [중식]",
-        "content": "크림소스스파게티\n닭강정\n양배추샐러드1.5.12.13.\n부쉬맨브레드1.2.5.6.13.\n버터 (1회용)2.5.13.\n피클&amp;단무지\n(음료) 과일맛음료 2",
-        "author": "중식",
-        "pub_date": "2019-11-29 00:00:00",
-        "reg_date": "2019-10-28 22:44:16",
-        "like_count": 0,
-        "share_count": 0,
-        "scrap_count": 0,
-        "comment_count": 0,
-        "images": null,
-        "video": null,
-        "files": null,
-        "created_at": "2019-10-28 22:44:16",
-        "updated_at": "2019-10-28 22:44:17",
-        "deleted_at": null,
-        "version": "2.0.0",
-        "article_status": 1,
-        "article_type": "article",
-        "view_link": "http://l.iamschool.net/articles/view/104470953",
-        "local_date_of_pub_date": "2019.11.29",
-        "day_of_week_of_pub_date": "금"
-      },
-      {
-        "id": 103645901,
-        "organization_id": 17195,
-        "organization_name": "선린인터넷고등학교",
-        "organization_logo": "https://iam-organization-r.akamaized.net/logo-image/17195/qa8AluKbzFCNKhOV.png",
-        "group_id": 2071367,
-        "group_name": "오늘의 급식",
-        "title": "11월 27일 [중식]",
-        "content": "칼슘쌀밥\n미니짬뽕9.13.\n훈제오리겨자무침1.\n알감자조림\n포기김치\n황도",
-        "author": "중식",
-        "pub_date": "2019-11-27 00:00:00",
-        "reg_date": "2019-10-24 01:31:29",
-        "like_count": 0,
-        "share_count": 0,
-        "scrap_count": 0,
-        "comment_count": 0,
-        "images": null,
-        "video": null,
-        "files": null,
-        "created_at": "2019-10-24 01:31:30",
-        "updated_at": "2019-10-24 01:31:32",
-        "deleted_at": null,
-        "version": "2.0.0",
-        "article_status": 1,
-        "article_type": "article",
-        "view_link": "http://l.iamschool.net/articles/view/103645901",
-        "local_date_of_pub_date": "2019.11.27",
-        "day_of_week_of_pub_date": "수"
-      }
-    ]
-};
-*/
 import axios from "axios";
 import * as moment from "moment";
 import "moment-timezone";
 moment.tz.setDefault("Asia/Seoul");
+const allergyDict = require("./allergy.json");
 
 async function getTodayMeal() {
-  let returnData = [];
+  // 오늘, 어제 급식 가져오기
+  let returnData = []; // 배열
   try {
     let data = await axios.get(`https://school.iamservice.net/api/article/organization/17195/group/3318247?next_token=0`);
     let items = data.data.articles;
     Object.keys(items).forEach(key => {
       let item = items[key];
-      if (moment(new Date()).format("YYYY.MM.DD") == item.local_date_of_pub_date) {
+      let date = item.local_date_of_pub_date.replace(/\./g, "-");
+      if (moment(new Date()).format("YYYY-MM-DD") == date) {
+        // 오늘 급식
         returnData.push({
-          date: item.local_date_of_pub_date,
-          meal: item.content,
-          img: item.images[0]
+          date: date, // 날짜
+          meal: item.content, // 식단
+          img: item.images[0] // 급식 이미지
         });
-      } else if (moment(new Date().setDate(new Date().getDate() - 1)).format("YYYY.MM.DD") == item.local_date_of_pub_date) {
+      } else if (moment(new Date().setDate(new Date().getDate() - 1)).format("YYYY-MM-DD") == date) {
+        // 어제 급식
         returnData.push({
-          date: item.local_date_of_pub_date,
-          meal: item.content,
-          img: item.images[0]
+          date: date, // 날짜
+          meal: item.content, // 식단
+          img: item.images[0] // 급식 이미지
         });
       }
     });
@@ -96,37 +36,51 @@ async function getTodayMeal() {
 }
 
 async function getAllMeal(maxToken: number) {
-  let returnData = [];
+  // 전체 급식 메뉴 가져오기
+  let returnData = {}; // JSON
   try {
     for (let currentToken = 0; currentToken <= maxToken; currentToken += 20) {
       let data = await axios.get(`https://school.iamservice.net/api/article/organization/17195/group/2071367?next_token=${currentToken}`);
       let items = data.data.articles;
       Object.keys(items).forEach(key => {
         let item = items[key];
-        let allergy = item.content.match(/\d/g);
-        allergy = allergy.join(", ");
-        console.log(allergy);
-        returnData[item.local_date_of_pub_date] = {
-          date: item.local_date_of_pub_date,
-          meal: item.content,
-          allergy: allergy
+        // 식단 형식화
+        let meal = item.content.replace(/\n/g, "<br>");
+        // 알레르기 정보
+        let allergy = item.content.match(/\d+\./g);
+        let allergyList = [];
+        allergy = [...new Set(allergy)]; // 중복 제거
+        allergy.sort((a, b) => a - b);
+        for (let i in allergy) {
+          allergy[i] = allergy[i].replace(/\./g, "");
+          allergyList.push(allergyDict[allergy[i]]);
+        }
+        allergy = allergy.join(" / "); // 배열을 문자열로 나열
+        //
+        let date = item.local_date_of_pub_date.replace(/\./g, "-");
+        returnData[date] = {
+          date: date,
+          meal: meal,
+          allergy: allergy,
+          allergyList: allergyList
         };
       });
-      return returnData;
     }
+    return returnData;
   } catch (err) {
     throw err;
   }
-  return returnData;
 }
 async function getMeal(method: string) {
   if (method === "/") {
+    // 오늘, 어제 급식 가져오기
     try {
       return await getTodayMeal();
     } catch (err) {
       throw err;
     }
   } else if (method === "monthly") {
+    // 전체 급식 가져오기
     try {
       return await getAllMeal(40);
     } catch (err) {
